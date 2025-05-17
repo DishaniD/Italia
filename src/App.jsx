3d83362c61b8fe40f6fce9a4518c54f62aac46bd
Dishani Dilrukshi <dilrukshi.dishani@gmail.com>
2025-05-17 19:47:55 +0400

    dishes updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import HeroSection from './Components/HeroSection'
+import Navbar from './Components/Navbar'
+import DishCard from './Components/DishCard'
+import Dishes from './Components/Dishes'
 
 const App = () => {
   return (
+<main className='overflow-y-hidden text-neutral-200 antialiased'> 
+<HeroSection/>
+<Navbar/>
+<Dishes/>
 
-      <div>
-        <h1 className="text-3xl font-bold underline bg-white">
-           Hello world! + Hi world
-        </h1>
-      </div>
+</main>
   )
 }
 
